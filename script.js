e50c89be8d943850ff692769bda074b8ae988301
kskannandotwin <kskannan.win@gmail.com>
2024-01-24 05:36:49 +0530

    24.01.2024-rp
    completed

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
-function addFourAges(a, b, c, d) {
-  return a + b + c + d;
+//ES5
+function isFullAge5() {
+  //console.log(arguments);
+  var argsArr = Array.prototype.slice.call(arguments);
+
+  argsArr.forEach(function (cur) {
+    console.log(2016 - cur >= 18);
+  });
 }
 
-var sum1 = addFourAges(18, 30, 12, 21);
-console.log(sum1);
-
-//ES5
-var ages = [18, 30, 12, 21];
-var sum2 = addFourAges.apply(null, ages);
-console.log(sum2);
+//isFullAge5(1990, 1999, 1965);
+//isFullAge5(1990, 1999, 1965, 2016, 1987);
 
 //ES6
-const sum3 = addFourAges(...ages);
-console.log(sum3);
+function isFullAge6(...years) {
+  years.forEach((cur) => console.log(2016 - cur >= 18));
+}
 
-const familySmith = ["John", "Jane", "Mark"];
-const familyMiller = ["Mary", "Bob", "Ann"];
-const bigFamily = [...familySmith, "Lily", ...familyMiller];
-console.log(bigFamily);
+isFullAge6(1990, 1999, 1965, 2016, 1987);
 
-const h = document.querySelector("h1");
-const boxes = document.querySelectorAll(".box");
-const all = [h, ...boxes];
+//ES5
+function isFullAge5(limit) {
+  var argsArr = Array.prototype.slice.call(arguments, 1);
 
-Array.from(all).forEach((cur) => (cur.style.color = "purple"));
+  argsArr.forEach(function (cur) {
+    console.log(2016 - cur >= limit);
+  });
+}
+
+//isFullAge5(16, 1990, 1999, 1965);
+isFullAge5(1990, 1999, 1965, 2016, 1987);
+
+//ES6
+function isFullAge6(limit, ...years) {
+  years.forEach((cur) => console.log(2016 - cur >= limit));
+}
+
+isFullAge6(16, 1990, 1999, 1965, 2016, 1987);
